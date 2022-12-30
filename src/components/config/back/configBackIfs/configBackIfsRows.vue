9616bd4d355b358983cb86603e13d43ab83f5881
Semenov <semenov@mail.ru>
2022-12-30 11:38:50 +0400

    C-3 интерфейс под command back

--- src/components/config/back/configBackIfs/configBackIfsRows.vue
+++ src/components/config/back/configBackIfs/configBackIfsRows.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="wrapper_back-ifs">
-    <ButtonAddConfigBackIfs :modelCommand="modelCommand"/>
-    <ConfigBackIfsRows
-        v-for="(modelCommandIfs, index) in modelCommand.ifs"
-        :key="index"
-        :modelCommandIfs="modelCommandIfs"
-    />
-    {{ modelCommand }}
+  <div>
+    {{ modelCommandIfs.dataset }}
   </div>
-
 </template>
 
 <script lang="ts">
@@ -16,14 +9,17 @@
 import ButtonAddConfigBackIfs from "../button/buttonAddConfigBackIfs/buttonAddConfigBackIfs.vue";
 import {PropType} from "vue";
 import {ModelCommand} from "../../../../command/model/ModelCommand";
-import ConfigBackIfsRows from "./configBackIfsRows.vue";
+import {ModelCommandIfs} from "../../../../command/model/ModelCommandIfs";
 export default {
-  name: "ConfigBackIfs",
-  components: {ConfigBackIfsRows, ButtonAddConfigBackIfs},
+  name: "ConfigBackIfsRows",
+  components: {ButtonAddConfigBackIfs},
   props: {
     modelCommand: {
       type: Object as PropType<ModelCommand>
     },
+    modelCommandIfs: {
+      type: Object as PropType<ModelCommandIfs>
+    }
   }
 }
 </script>
